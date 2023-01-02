af5fd6a6d8f50adefe02d15f581617a9bb70d41e
Mark Wecke <wecke@neoskop.de>
2023-01-02 22:35:27 +0100

    feat(wiki): configure document/app

--- apps/wiki/pages/_app.tsx
+++ apps/wiki/pages/_app.tsx
@@ -1,12 +1,66 @@
+import { CacheProvider, EmotionCache, ThemeProvider } from '@emotion/react';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import { MDXProvider } from '@mdx-js/react';
+import { CssBaseline, Typography } from '@mui/material';
+import type { MDXComponents } from 'mdx/types';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
-const components = {};
+import { Link, THEME, createEmotionCache } from '@dragosia/ui';
 
-export default function App({ Component, pageProps }: AppProps) {
+const clientSideEmotionCache = createEmotionCache();
+const components: MDXComponents = {
+    a: ({ href = '#', target, children, id }) => <Link color="secondary" underline="none" {...{ href, target, children, id }} />,
+    h1: ({ id, children }) => (
+        <Typography variant="h2" component="h2" id={id} sx={{ mt: '1.6rem' }}>
+            {children}
+        </Typography>
+    ),
+    h2: ({ id, children }) => (
+        <Typography variant="h3" component="h3" id={id}>
+            {children}
+        </Typography>
+    ),
+    h3: ({ id, children }) => (
+        <Typography variant="h4" component="h4" id={id}>
+            {children}
+        </Typography>
+    ),
+    h4: ({ id, children }) => (
+        <Typography variant="h5" component="h5" id={id}>
+            {children}
+        </Typography>
+    ),
+    h5: ({ id, children }) => (
+        <Typography variant="h6" component="h6" id={id}>
+            {children}
+        </Typography>
+    ),
+    p: ({ children }) => (
+        <Typography variant="body1" component="p" sx={{ mb: '0.8rem' }}>
+            {children}
+        </Typography>
+    )
+};
+
+export default function App({ Component, pageProps, emotionCache = clientSideEmotionCache }: AppProps & { emotionCache?: EmotionCache }) {
     return (
-        <MDXProvider components={components}>
-            <Component {...pageProps} />
-        </MDXProvider>
+        <>
+            <CacheProvider value={emotionCache}>
+                <Head>
+                    <meta name="viewport" content="initial-scale=1, width=device-width" />
+                </Head>
+                <MDXProvider components={components}>
+                    <ThemeProvider theme={THEME}>
+                        <CssBaseline />
+                        <Component {...pageProps} />
+                    </ThemeProvider>
+                </MDXProvider>
+            </CacheProvider>
+        </>
     );
 }
