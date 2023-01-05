813441f28e0aaf88338fb3aacdf28908d2181a35
Mark Wecke <wecke@neoskop.de>
2023-01-05 13:45:45 +0100

    feat(ui): implement header texture as css gradient

--- packages/ui/page/Header.tsx
+++ packages/ui/page/Header.tsx
@@ -1,7 +1,7 @@
 import { AppBar, Box, Toolbar } from '@mui/material';
 import React from 'react';
 
-import TopWood from '../assets/top-wood.png';
+import Noise from '../assets/noise.svg';
 import { Link } from '../wiki';
 import { AuthMenu } from '../wiki/AuthMenu';
 
@@ -42,14 +42,43 @@ export const PageHeader: React.FunctionComponent<React.PropsWithChildren<{ title
                     {children}
                 </Toolbar>
             </AppBar>
-            <Box
-                sx={{
-                    width: '100%',
-                    height: '15px',
-                    backgroundImage: `url(${TopWood.src})`,
-                    backgroundRepeat: 'repeat-x',
-                    backgroundSize: 'contain'
-                }}></Box>
+            <Box sx={{ width: '100%', display: 'flex', '& > *': { flexGrow: 1 } }}>
+                <Box
+                    sx={{
+                        height: '15px',
+                        width: '100%',
+                        position: 'relative',
+                        backgroundImage: `url(${Noise.src})`,
+                        backgroundSize: '650px',
+                        '&:after': {
+                            content: '""',
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            right: 0,
+                            bottom: 0,
+                            background: `linear-gradient(
+                                to bottom,
+                                rgba(234, 218, 195, 0.9) 0%,
+                                rgba(227, 187, 147, 0.9) 5%,
+                                rgba(152, 102, 65, 0.9) 25%,
+                                rgba(23, 8, 4, 0.9) 30%,
+                                rgba(23, 8, 4, 0.9) 34%,
+                                rgba(64, 31, 5, 0.9) 37%,
+                                rgba(227, 187, 147, 0.9) 38%,
+                                rgba(152, 102, 65, 0.9) 53%,
+                                rgba(23, 8, 4, 0.9) 58%,
+                                rgba(64, 31, 5, 0.9) 67%,
+                                rgba(227, 187, 147, 0.9) 68%,
+                                rgba(152, 102, 65, 0.9) 74%,
+                                rgba(23, 8, 4, 0.9) 76%,
+                                rgba(23, 8, 4, 0.9) 100%
+                            )`,
+                            mixBlendMode: 'hard-light'
+                        },
+                        filter: 'contrast(100%) brightness(60%) saturate(200%)'
+                    }}></Box>
+            </Box>
         </Box>
     );
 };
